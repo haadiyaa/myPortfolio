f79c59cf8dcdca47512eb79f6d8b1b69e6cd7e3c
haadiyaa <haadiyact@gmail.com>
2024-07-11 16:15:19 +0530

    first commit

---
+++ lib/resource/appClass.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:portfolio/model/experienceModel.dart';
+import 'package:portfolio/resource/styles.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+enum ScreenType { mobile, tab, web }
+
+class AppClass {
+  static final AppClass _mAppClass = AppClass._internal();
+  static BuildContext? lastContext;
+  ScrollController controller = ScrollController();
+
+  /* URL */
+  static final resumeDownloadURL =
+      '''https://github.com/haadiyaa/eventeaze/blob/master/assets/google1.png''';
+
+  static final gitEZ = '''https://github.com/haadiyaa/eventeaze''';
+
+  static final gitWeather = '''https://github.com/haadiyaa/weather_app''';
+  static final gitTodo = '''https://github.com/haadiyaa/todo_app''';
+  static final gitGemini = '''https://github.com/haadiyaa/geminibot''';
+  static final gitStudent = '''https://github.com/haadiyaa/student-details''';
+
+
+  List<WorkModel> projectList = [
+    WorkModel(
+        projectTitle: "EventEaze",
+        projectContent:
+            '''A Flutter Event Management App built using Firebase and Bloc state management. With a user-friendly interface, EventEaze allows event organizers to create and manage events effortlessly while providing attendees with easy access to events.
+
+Features
+- Users can discover events by categories .
+- Create paid and free events with detailed description and image. They can also edit and delete their events.
+- Search functionality for discovering events.
+- Users can share the event details with friends and on social media platforms directly for the app.
+- The event organizers get notifications when the attendees join the event.''',
+        tech1: "Flutter",
+        tech2: "Firebase",
+        tech3: "BLoC"),
+    WorkModel(
+      projectTitle: "Weather App",
+      projectContent:
+          '''A Flutter application built with flutter and provider state management, which utilize http for API integration. It provides the current weather data.
+
+Features
+- Fetch curent location: Provides weather data according to the user’s current location
+- Search other city’s weather: user can search any other city to get the weather of that city.''',
+      tech1: "Flutter",
+      tech2: "API",
+      tech3: "Provider",
+    ),
+    WorkModel(
+      projectTitle: "To-do App",
+      projectContent:
+          '''A Flutter Todo app which is designed to manage your tasks efficiently and effectively, utilizing bloc statemanagement and local storage.
+
+Features
+- Light and Dark Themes: Customise the look to suit your preference.
+- Comprehensive Task Management: Add, edit, delete, and update your tasks with ease.
+- Organized Task View: Separate views for pending and completed tasks to keep you organized.
+          ''',
+      tech1: "Flutter",
+      tech2: "BLoC",
+    ),
+    WorkModel(
+      projectTitle: "Gemini Chatbot",
+      projectContent:
+          '''A Chatbot application using Gemini API made with flutter. Uses Provider State management. User can ask any doubts only text-input is supported''',
+      tech1: "Flutter",
+      tech2: "Gemini API",
+      tech3: "Provider",
+    ),
+    WorkModel(
+        projectTitle: "Student Management App",
+        projectContent:
+            '''A Flutter application for storing and displaying student details using SQFlite as database and implementing the MVC architecture.
+
+Features
+- Image picker : Image picking functionality for student profile picture.
+- Management details: Add, edit and delete the students details.''',
+        tech1: "Flutter",
+        tech2: "SQFlite",
+        tech3: "MVC"),
+  ];
+
+  factory AppClass() {
+    return _mAppClass;
+  }
+
+  AppClass._internal();
+
+  getMqWidth(BuildContext context) {
+    return MediaQuery.of(context).size.width;
+  }
+
+  getMqHeight(BuildContext context) {
+    return MediaQuery.of(context).size.height;
+  }
+
+  showSnackBar(String msg, {BuildContext? context}) {
+    ScaffoldMessenger.of(context ?? lastContext!)
+        .showSnackBar(SnackBar(content: Text(msg)));
+  }
+
+  ScreenType getScreenType(BuildContext context) {
+    double scrWidth = getMqWidth(context);
+    if (scrWidth > 915) {
+      return ScreenType.web;
+    } else if (scrWidth < 650) {
+      return ScreenType.mobile;
+    }
+    return ScreenType.tab;
+  }
+
+  downloadResume(context) async {
+    await launchUrl(Uri.parse(AppClass.resumeDownloadURL));
+  }
+
+  alertDialog(context, title, msg) {
+    showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+                title: Text(title, style: TxtStyle().boldWhite(context)),
+                content: Text(msg),
+                actions: [
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green),
+                      onPressed: () => Navigator.pop(context),
+                      child: Text('Okay'))
+                ]));
+  }
+
+  Future<bool> sendEmail(name, contact, msg) async {
+    var url = Uri.https('hbk-portfolio-mailer.web.app', '/sendMail');
+    var response = await post(url,
+            body: {"name": name, "contactInfo": contact, "message": msg})
+        .timeout(Duration(seconds: 10));
+    print(response.body);
+    return response.statusCode == 200;
+  }
+}
---
+++ lib/resource/strings.dart
@@ -0,0 +1,51 @@
+class Strings {
+  /* Intro screen contents */
+  static final welcomeTxt = '''Hi, my name is''';
+  static final name = '''Haadiya.'''; // add . at the end
+  static final whatIdo = '''I build things for the mobile/web.'''; // add . at the end
+  static final introAbout =
+      '''I turn ideas into reality.  As a Flutter developer with a deep understanding of UI/UX principles, I build user-centric mobile apps that are not only visually appealing but also efficient and scalable. I use Flutter's features to create seamless, performant applications that bring your vision to life.''';
+
+  /* About screen contents */
+  static final aboutPara1 =
+      '''My interest with designing user interfaces (UI) led me to the world of mobile app development. I enhanced my Flutter app development skills through self-directed learning, driven by a passion to create user-centric apps.''';
+  static final aboutPara2 =
+      '''My dedication to learning translated into hands-on experience. I've actively developed projects that have established my knowledge of state management, key Flutter concepts, and problem solving.  This solid foundation allows me to face issues with confidence and produce effective solutions.''';
+  static final aboutPara3 = '''I also freelance for various clients across the world. If you've any ideas about creating a project or something, feel free to contact me!''';
+
+  static final techIntro = '''Here are some core Flutter areas I've been focusing on recently:''';
+  static final tech1 = ''' Bloc'''; // space for manual padding
+  static final tech2 = ''' Provider'''; // space for manual padding
+  static final tech3 = ''' GetX'''; // space for manual padding
+  static final tech4 = ''' Firebase'''; // space for manual padding
+  static final tech5 = ''' Hive'''; // space for manual padding
+  static final tech6 = ''' SQFlite'''; // space for manual padding
+  static final tech7 = ''' Git'''; // space for manual padding
+  static final tech8 = ''' Api Integration'''; // space for manual padding
+
+  /* Exp screen contents */
+  static final expCompName1 = '''Self Stack''';
+  static final expDesig1 = '''Flutter Intern''';
+  static final expDur1 = '''Nov 2019 - Mar 2020''';
+  static final expAbout1 = '''To kick start my carrier in IT, joined as an Intern to gain knowledge and experience in software industry, Mobile Application Development.''';
+  static final expAbout1_2 = '''Learned and explored native Android architecture models, network calls and core functionality.''';
+  static final expAbout1_3 = '''Has worked on several live projects on different platforms such as e-commerce, ERP, CMS Applications.''';
+
+  static final expCompName2 = '''Rax-Tech International''';
+  static final expDesig2 = '''Mobile App Developer''';
+  static final expDur2 = '''Aug 2020 - Jun 2022''';
+  static final expAbout2 = '''Worked as a senior mobile app developer and maintained a team of Freshers and interns to provide a hands-on solution.''';
+  static final expAbout2_2 = '''Design and maintenance of the key functions of IoT (Internet of Things), Erp, CMS and innovative applications.''';
+  static final expAbout2_3 = '''Experienced many technologies such as wifi (TCP/UDP), BLE, MQTT, LIVE DATA, Google ML - Tensor Flow, GraphQL and so on.''';
+
+  static final expCompName3 = '''Self Stack''';
+  static final expDesig3 = '''Flutter Intern''';
+  static final expDur3 = '''Sep 2022 - Present''';
+  static final expAbout3 = '''An entity of India's leading business conglomerate Murugappa Group of companies.''';
+  static final expAbout3_2 = '''Worked with a team of 5 engineers to create field force automation applications, business-to-business applications, etc.''';
+  static final expAbout3_3 = '''Interfacing with customers on a weekly basis, delivering technology expertise.''';
+
+  /* Contact screen content */
+  static final endTxt =
+      '''Although I’m looking forward to any new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!''';
+}
